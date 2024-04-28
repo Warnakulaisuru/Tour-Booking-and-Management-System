90b2059aa4000a32f45ff13c7b4d7e62f7bf5ae8
Warnakulaisuru <warnakulaisuru@gmail.com>
2024-04-28 23:59:43 +0530

    installing axios and create events for registration form

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
+// const cors = require('cors');
 const app = express();
 
-app.get('/test',(req,res)=>{
-    res.json('test ok');
-}
-);
+// app.use(cors({
+//     credentials:true,
+//     origin:'http://localhost:5174',
+// }));
+
+app.get("/test", (req, res) => {
+  res.json("test ok");
+});
 
 app.listen(4000);
