eaf1af3da7180f202d3103f7ce050f5558d85d02
Warnakulaisuru <warnakulaisuru@gmail.com>
2024-04-29 19:41:47 +0530

    connect frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-// const cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
-// app.use(cors({
-//     credentials:true,
-//     origin:'http://localhost:5174',
-// }));
+app.use(cors({
+    credentials:true,
+    origin:'http://192.168.0.101:5173',
+}))
 
-app.get("/test", (req, res) => {
-  res.json("test ok");
+app.get('/test', (req,res) => {
+    res.json('test ok');
 });
 
 app.listen(4000);
